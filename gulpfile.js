a1f60df5f4bc8c2a4dbce2b99d3d0cafa62e87ce
Caner Uguz <caneruguz@gmail.com>
2014-08-14 16:05:53 -0400

    Cleaning up and fixing bugs
    Fixed the xs issue by providing 12 col defaults

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,43 @@ var concat = require("gulp-concat")
 var minifyCSS = require('gulp-minify-css');
 var less = require('gulp-less');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
 var paths = {
     cssfiles : ["./bower_components/bootstrap/dist/css/*.min.css", "./bower_components/jquery-ui/themes/ui-lightness/*.min.css", "./css/*.css"],
     jsfiles : [ "./bower_components/jquery/dist/*.min.js", "./bower_components/jquery-ui/*.min.js", "./bower_components/bootstrap/dist/js/*.min.js", "./scripts/*.js" ],
 }
 
-
-
 gulp.task('css', function(){
     return gulp.src(paths.cssfiles)
         .pipe(concat('bundle.css'))
         .pipe(minifyCSS({keepBreaks:true}))
-        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest('./demo/'))
 });
 
 gulp.task('js', function(){
     return gulp.src(paths.jsfiles)
         .pipe(concat('bundle.js'))
-        .pipe(gulp.dest('./dist/'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./demo/'))
 });
+
+gulp.task('dist_full', function(){
+    return gulp.src('./scripts/scripts.js')
+            .pipe(rename('jquery-rescon.js'))
+            .pipe(gulp.dest('./dist/'))
+})
+gulp.task('dist_min', ['dist_full'], function(){
+    return gulp.src('./scripts/scripts.js')
+            .pipe(uglify())
+            .pipe(rename('jquery-rescon.min.js'))
+            .pipe(gulp.dest('./dist/'))
+})
 
 gulp.task('watch', function() {
     gulp.watch(paths.cssfiles, ['css']);
     gulp.watch([paths.jsfiles, './scripts/script.js'], ['js']);
 });
 
-gulp.task("default", ["css", "js", "watch"]);
+gulp.task("default", ["css", "js", "dist_min", "watch"]);
